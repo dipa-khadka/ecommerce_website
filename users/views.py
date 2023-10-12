fd16c5789986127968d0a098be4aefa4a77fd3a1
dipa-khadka <dipshanakhadka77@gmail.com>
2023-10-12 14:00:14 +0545

    login & register

--- users/views.py
+++ users/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "index.html")
+
+def user_login(request):
+    return render(request, "login.html")
+
+def user_register(request):
+    return render(request, "register.html")
